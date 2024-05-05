5196f9472ebbe7641f0211700081526e94add011
AnTaraXia7742 <bagussadewo7742@gmail.com>
2024-05-06 05:19:40 +0700

    responsive

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
 menu.onclick = () => {
-  menu.classList.toggle('fa-times');
+  menu.classList.toggle('fa-times'); // Corrected: Remove space before 'fa-times'
   navbar.classList.toggle('active');
 };
 
 window.onscroll = () => {
-  menu.classList.remove(fa - times);
-  navbar.classList.toggle('active');
+  // Consider adding logic here to conditionally toggle 'active' based on scroll position
+  // Example:
+  if (window.scrollY > 100) {
+    // Hide navbar when scrolled past 100px
+    menu.classList.remove('fa-times'); // Remove 'fa-times' if present
+    navbar.classList.remove('active');
+  } else {
+    // Show navbar when scrolled back to the top
+  }
 };
 
 var swiper = new Swiper('.home-slider', {
